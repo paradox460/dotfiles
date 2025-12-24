3ca3c8e35dfef361cbc97bf4bfb552fc70355798
Jeff Sandberg <jeff@pdx.su>
2025-12-23 23:08:53 -0700

    nvim: fix markdown config indents

--- dot_config/nvim/exact_lua/exact_plugins/markdown.lua
+++ dot_config/nvim/exact_lua/exact_plugins/markdown.lua
@@ -15,10 +15,10 @@ return {
     config = function()
       -- Disable folding by default (can be slow on large files)
       vim.g["pandoc#folding#enabled"] = 0
-      
+
       -- Use Treesitter for syntax highlighting if available
       vim.g["pandoc#syntax#use_treesitter"] = 1
-      
+
       -- Disable spell checking (let user enable manually)
       vim.g["pandoc#spell#enabled"] = 0
     end,
@@ -35,7 +35,7 @@ return {
     config = function()
       -- Use | as corner character for markdown-style tables
       vim.g.table_mode_corner = "|"
-      
+
       -- Auto-format tables as you type
       vim.g.table_mode_auto_align = 1
     end,
